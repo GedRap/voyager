a91a1995ee7761a6d340723b3e325673ac0f5d48
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2014-07-29 22:52:46 +0100

    Test for Portfolio.execute_for_day

--- test/test_portfolio.py
+++ test/test_portfolio.py
@@ -46,12 +46,48 @@ class PortfolioTest(unittest.TestCase):
         self.portfolio.update_cash_with_order(self.sell_aapl_order)
         self.assertEqual(self.portfolio.cash_ts["2011-01-14"],980902)
 
+    # This case is more of an integration test,
+    # its components were tested separately
+    def test_process_day(self):
+        date0110 = datetime(2011,1,10,23) # buy aapl
+        date0111 = datetime(2011,1,11,23) # buy ibm
+        date0112 = datetime(2011,1,12,23) # sell aapl
+        date0113 = datetime(2011,1,13,23) # no orders on this
+        date0114 = datetime(2011,1,14,23) # sell ibm
+
+        self.add_all_orders()
+
+        self.assertEqual(self.get_long_holding("AAPL"), 0)
+
+        orders = self.portfolio.process_day(date0110)
+        self.assertEqual(self.get_long_holding("AAPL"), 100)
+        self.assertEqual(self.get_long_holding("IBM"), 0)
+
+        self.portfolio.process_day(date0111)
+        self.assertEqual(self.get_long_holding("AAPL"), 100)
+        self.assertEqual(self.get_long_holding("IBM"), 15)
+
+        self.portfolio.process_day(date0112)
+        self.assertEqual(self.get_long_holding("AAPL"), 50)
+        self.assertEqual(self.get_long_holding("IBM"), 15)
+
+        self.portfolio.process_day(date0113)
+        self.assertEqual(self.get_long_holding("AAPL"), 50)
+        self.assertEqual(self.get_long_holding("IBM"), 15)
+
+        self.portfolio.process_day(date0114)
+        self.assertEqual(self.get_long_holding("AAPL"), 50)
+        self.assertEqual(self.get_long_holding("IBM"), 0)
+
     #helper method
     def add_all_orders(self):
-        self.portfolio.add_future_order(self.buy_ibm_order)
-        self.portfolio.add_future_order(self.buy_aapl_order)
-        self.portfolio.add_future_order(self.sell_aapl_order)
-        self.portfolio.add_future_order(self.sell_ibm_order)
+        self.portfolio.add_future_order(self.buy_aapl_order) #100
+        self.portfolio.add_future_order(self.buy_ibm_order) #15
+        self.portfolio.add_future_order(self.sell_aapl_order) #-50
+        self.portfolio.add_future_order(self.sell_ibm_order) #-15
 
+    def get_long_holding(self, symbol):
+        position_long = self.portfolio.holding.POSITION_LONG
+        return self.portfolio.holding.get_latest_holding_amount(position_long, symbol)
 if __name__ == '__main__':
     unittest.main()
